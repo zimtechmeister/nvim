a07aeaaa6dbccf8f18bafd27e934893756c1ef4f
Tim Zechmeister <tim.zechmeister03@gmail.com>
2025-01-22 01:46:40 +0100

    added copilot icon(not using mini.icons)

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -4,16 +4,16 @@ return {
         dependencies = {
             "rafamadriz/friendly-snippets",
             "giuxtaposition/blink-cmp-copilot",
-    {
-      "zbirenbaum/copilot-cmp",
-      dependencies = {
-        "zbirenbaum/copilot.lua",
-        {
-          "saghen/blink.compat",
-          opts = { impersonate_nvim_cmp = true, enable_events = true },
-        },
-      },
-    },
+            {
+                "zbirenbaum/copilot-cmp",
+                dependencies = {
+                    "zbirenbaum/copilot.lua",
+                    {
+                        "saghen/blink.compat",
+                        opts = { impersonate_nvim_cmp = true, enable_events = true },
+                    },
+                },
+            },
         },
 
         version = '*',
@@ -23,34 +23,11 @@ return {
 
             completion = {
                 menu = {
-                    -- mini icons
-                    draw = {
-                        components = {
-                            kind_icon = {
-                                ellipsis = false,
-                                text = function(ctx)
-                                    local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
-                                    return kind_icon
-                                end,
-                                -- Optionally, you may also use the highlights from mini.icons
-                                highlight = function(ctx)
-                                    local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
-                                    return hl
-                                end,
-                            }
-                        }
-                    },
-
                     border = 'single'
                 },
                 documentation = { window = { border = 'single' } },
             },
             signature = { window = { border = 'single' } },
-
-            appearance = {
-                use_nvim_cmp_as_default = true,
-                nerd_font_variant = 'mono'
-            },
 
             sources = {
                 default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
@@ -60,10 +37,70 @@ return {
                         module = "blink-cmp-copilot",
                         score_offset = 100,
                         async = true,
+
+                        -- Copilot kind icons
+                        transform_items = function(_, items)
+                            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+                            local kind_idx = #CompletionItemKind + 1
+                            CompletionItemKind[kind_idx] = "Copilot"
+                            for _, item in ipairs(items) do
+                                item.kind = kind_idx
+                            end
+                            return items
+                        end,
                     },
+                },
+            },
+
+            appearance = {
+                use_nvim_cmp_as_default = true,
+                nerd_font_variant = 'mono',
+
+                -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
+                kind_icons = {
+                    Copilot = '',
+                    Text = '󰉿',
+                    Method = '󰊕',
+                    Function = '󰊕',
+                    Constructor = '󰒓',
+
+                    Field = '󰜢',
+                    Variable = '󰆦',
+                    Property = '󰖷',
+
+                    Class = '󱡠',
+                    Interface = '󱡠',
+                    Struct = '󱡠',
+                    Module = '󰅩',
+
+                    Unit = '󰪚',
+                    Value = '󰦨',
+                    Enum = '󰦨',
+                    EnumMember = '󰦨',
+
+                    Keyword = '󰻾',
+                    Constant = '󰏿',
+
+                    Snippet = '󱄽',
+                    Color = '󰏘',
+                    File = '󰈔',
+                    Reference = '󰬲',
+                    Folder = '󰉋',
+                    Event = '󱐋',
+                    Operator = '󰪚',
+                    TypeParameter = '󰬛',
                 },
             },
         },
         opts_extend = { "sources.default" }
     },
+
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({})
+        end,
+    }
 }
