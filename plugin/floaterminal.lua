a61f5a7f20f88487455aae0f7403975d7bbabfa9
Tim Zechmeister <tim.zechmeister03@gmail.com>
2025-02-03 00:36:34 +0100

    friendship ended with telescope now Snacks.picker is my best friend & better keymaps

--- plugin/floaterminal.lua
+++ plugin/floaterminal.lua
@@ -56,4 +56,4 @@ end
 -- Example usage:
 -- Create a floating window with default dimensions
 vim.api.nvim_create_user_command("Floaterminal", toggle_terminal, {})
-vim.keymap.set({ "n", "t" }, "<leader>tt", toggle_terminal, { desc = "Toggle FloaTerminal" })
+vim.keymap.set({ "n", "t" }, "<leader><leader>", toggle_terminal, { desc = "Toggle FloaTerminal" })
