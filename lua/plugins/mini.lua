35e7ad1e6c329ea889e57a81df5f5dfeb58e9db3
Tim Zechmeister <tim.zechmeister03@gmail.com>
2025-04-18 19:14:15 +0200

    swap comment.nvim with builtin mini plugin(dont even know what it really does)

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,6 +3,7 @@ return {
         'echasnovski/mini.nvim',
         version = false,
         config = function()
+            require('mini.comment').setup()
             require('mini.ai').setup()
             require('mini.icons').setup()
             require('mini.pairs').setup()
